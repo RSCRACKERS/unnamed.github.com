9c0ad978a3a7650646ec614198baaae20ede17ad
ROSHAN SRI <123759991+RSCRACKERS@users.noreply.github.com>
2023-09-15 23:46:01 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,7 @@
+// JavaScript for the responsive menu
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('closed');
+});
